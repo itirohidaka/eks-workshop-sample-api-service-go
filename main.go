61ef3440d95123802e40f1e2f7b115b099d00728
Itiro <itirohidaka@gmail.com>
2020-05-14 06:42:50 -0300

    main.go
    changed

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World - DXC"}
+		res := &response{Message: "Ola time DXC"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
