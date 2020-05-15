c4141303a57012e2c03519a8ea1e2e087bd58e96
Itiro <itirohidaka@gmail.com>
2020-05-15 08:47:36 -0300

    Message changed
    Message changed

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello-world-itiro"}
+		res := &response{Message: "Hello World Itiro Hidaka"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
