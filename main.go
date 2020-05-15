da1a9976efbb7cb34563999f8dde1499efbe90b5
Itiro <itirohidaka@gmail.com>
2020-05-14 22:40:10 -0300

    teste
    teste

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Ola time DXC"}
+		res := &response{Message: "Hello-world"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
