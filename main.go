a04c15533e823a05a7fb431faba37d93862fe78a
Itiro <itirohidaka@gmail.com>
2020-08-18 15:39:17 -0300

    BlackBelt example

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World Itiro 123"}
+		res := &response{Message: "Hello World Itiro BlackBelt"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
